453231a05fe2cc1de9352ec932e1cd228bdd0505
Nathan Zadoks <nathan@nathan7.eu>
2013-07-07 15:09:24 +0200

    refactor

--- index.js
+++ index.js
@@ -2,29 +2,20 @@ var Promise = require('promise')
 
 module.exports = exports =
 function promizeResolved(value) {
-  return new Promise(function(resolve, reject) { resolve(value) })
+  return Promise.from(value)
 }
-
+exports.then = exports().then
 exports.resolved = exports
-
-exports.then = function(onFulfill, onReject) {
-  return exports().then(onFulfill, onReject)
-}
 
 exports.rejected =
 function promizeRejected(value) {
   return new Promise(function(resolve, reject) { reject(value) })
 }
+exports.rejected.then = exports.rejected().then
 
-exports.rejected.then = function(onFulfill, onReject) {
-  return exports.rejected().then(onFulfill, onReject)
-}
 
 exports.never =
 function promizeNever() {
   return new Promise(function(resolve, reject) {})
 }
-
-exports.never.then = function(onFulfill, onReject) {
-  return exports.never()
-}
+exports.never.then = exports.never().then
